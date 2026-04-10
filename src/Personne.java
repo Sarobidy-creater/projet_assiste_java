76ef1133901084d334f1a849be872d56cd04ffa5
Sarobidy-creater <irintso.saa@gmail.com>
2026-04-10 10:08:18 +0200

    class mère Personne

---
+++ src/Personne.java
@@ -0,0 +1,42 @@
+public class Personne {
+    public int Identifiant;
+    public String Nom;
+    public String Adresse;
+    public String Contact;
+
+
+    public Personne(int Identifiant, String Nom, String Adresse, String Contact){
+        this.Identifiant = Identifiant;
+        this.Nom = Nom;
+        this.Adresse = Adresse;
+        this.Contact = Contact;
+    }
+    public String obtenirInfos(){
+        return "Informations : "+ "\nIdentifiant : " + getIdentifiant() +
+                "\n Nom : " + getNom() +
+                "\n Adresse : " + getAdresse() +
+                "\nContact : " + getContact();
+    };
+
+    public String getNom(){
+        return Nom;
+    };
+
+    public String getAdresse(){
+        return Adresse;
+    };
+
+    public int getIdentifiant(){
+        return Identifiant;
+    };
+
+    public String getContact() {
+        return Contact;
+    };
+
+    public static void main(String[] args){
+        Personne clemence = new Personne(111, "clemence", "2 rue billancourt", "0762752498");
+        System.out.println(clemence.obtenirInfos());
+
+    };
+}
